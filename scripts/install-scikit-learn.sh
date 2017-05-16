7e4e42d6811125e90e9271d15ea42911e54e0be6
Paul Lin <goodseeyou@localhost.localdomain>
2017-05-16 14:30:12 +0000

    setup sklean

---
+++ scripts/install-scikit-learn.sh
@@ -0,0 +1,7 @@
+sudo yum install epel-release -y \
+    && sudo yum install python-pip -y \
+    && sudo pip install --upgrade pip \
+    && pip install -U scikit-learn 
+
+sudo yum install python-devel -y
+
